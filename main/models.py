95d9b400cfc1c574748eb3965875199099263da9
s7ntpenh <denismazur111@gmail.com>
2025-05-02 14:16:36 +0200

    first

--- main/models.py
+++ main/models.py
@@ -15,10 +15,14 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
+
 
 class Student(models.Model):
     name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)  # нове поле
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.name
