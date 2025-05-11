83e5a502a0a1783cc495de37e2572b7c0ec68862
s7ntpenh <denismazur111@gmail.com>
2025-05-11 12:12:45 +0200

    first commit

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main import views
+from main.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homework_querysets, name='homework_querysets'),
 ]
